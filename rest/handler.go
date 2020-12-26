40f2c45fa9d45b81ec81912e5aee56c618de5252
ichxxx <czn16@qq.com>
2020-12-27 02:58:33 +0800

    [+] Add rest/resp and rest/handler

---
+++ rest/handler.go
@@ -0,0 +1,21 @@
+package rest
+
+import (
+	"shack"
+)
+
+// NotFoundHandler returns a handler func to respond to non existent routes with a REST compliant
+// error message
+func NotFoundHandler() shack.HandlerFunc {
+	return func(ctx *shack.Context) {
+		ctx.JSON(Resp().Error("resource not found"))
+	}
+}
+
+// MethodNotAllowedHandler returns a handler func to respond to routes requested with the wrong verb a
+// REST compliant error message
+func MethodNotAllowedHandler() shack.HandlerFunc {
+	return func(ctx *shack.Context) {
+		ctx.JSON(Resp().Error("method not allowed"))
+	}
+}
---
+++ rest/resp.go
@@ -0,0 +1,137 @@
+package rest
+
+import (
+	"fmt"
+	"reflect"
+	"strconv"
+	"sync"
+)
+
+var (
+	okC      = 0
+	okM      = "success"
+	failC    = 1
+	failM    = "fail"
+	respPool = &sync.Pool{New: func() interface{}{return new(resp)}}
+)
+
+type resp struct {
+	C  int          `json:"code,omitempty"`
+	M  string       `json:"msg,omitempty"`
+	E  interface{}  `json:"error,omitempty"`
+	D  map[string]interface{}
+}
+
+
+func Resp() *resp {
+	r := respPool.Get().(*resp)
+	r.reset()
+	respPool.Put(r)
+	return r
+}
+
+
+func(r *resp) OK() *resp {
+	r.C = okC
+	r.M = okM
+	return r
+}
+
+
+func(r *resp) Fail() *resp {
+	r.C = failC
+	r.M = failM
+	return r
+}
+
+
+func(r *resp) Code(code int) *resp {
+	r.C = code
+	return r
+}
+
+
+func(r *resp) Msg(msg string) *resp {
+	r.M = msg
+	return r
+}
+
+
+func(r *resp) Error(error interface{}) *resp {
+	r.E = error
+	return r
+}
+
+
+func(r *resp) Data(kv ...interface{}) *resp {
+	l := len(kv)
+	for i := 1; i < l; i+=2 {
+		r.D[str(kv[i-1])] = kv[i]
+	}
+
+	return r
+}
+
+
+func(r *resp) DefaultOkCode(code int) {
+	okC = code
+}
+
+
+func(r *resp) DefaultOkMsg(msg string) {
+	okM = msg
+}
+
+
+func(r *resp) DefaultFailCode(code int) {
+	failC = code
+}
+
+
+func(r *resp) DefaultFailMsg(msg string) {
+	failM = msg
+}
+
+
+func(r *resp) reset() {
+	r.C = 0
+	r.M = r.M[0:0]
+	r.E = nil
+	r.D = make(map[string]interface{})
+}
+
+
+func str(i interface{}) string {
+	switch i.(type) {
+	case string:
+		return i.(string)
+	case int:
+		return strconv.Itoa(i.(int))
+	case uint:
+		return strconv.FormatUint(uint64(i.(uint)), 10)
+	case int64:
+		return strconv.Itoa(int(i.(int64)))
+	case uint64:
+		return strconv.FormatUint(i.(uint64), 10)
+	case int32:
+		return strconv.Itoa(int(i.(int32)))
+	case uint32:
+		return strconv.FormatUint(uint64(i.(uint32)), 10)
+	case int16:
+		return strconv.Itoa(int(i.(int16)))
+	case uint16:
+		return strconv.FormatUint(uint64(i.(uint16)), 10)
+	case int8:
+		return strconv.Itoa(int(i.(int8)))
+	case float64:
+		return strconv.FormatFloat(i.(float64), 'E', -1, 64)
+	case float32:
+		return strconv.FormatFloat(float64(i.(float32)), 'E', -1, 64)
+	case byte:
+		return string(i.(byte))
+	case []byte:
+		return string(i.([]byte))
+	default:
+		panic(fmt.Sprintf("shack: can't convert %v to string", reflect.TypeOf(i)))
+	}
+}
